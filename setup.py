6f31f2c73eec49306ec0c56a788eb5c6fa3b5279
Margarita Rudenskaya <margarita.teplyakova@gmail.com>
2017-12-05 14:34:41 +0700

    update setup file

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(name='cryptocompy',
       packages=['cryptocompy'],
-      version='0.1.1.dev1',
+      version='0.1.1.dev2',
       description='Simple wrapper for the public Cryptocompare API.',
       keywords = '',
       author='Titian Steiger',
       author_email='titian.steiger@gmail.com',
-      url='https://github.com/ttsteiger/cryptocompare-python',
-      download_url='https://github.com/ttsteiger/cryptocompare-python/archive/0.1.1.dev1.tar.gz',
+      url='https://github.com/wildsmileofbutterfly/cryptocompy',
+      download_url='https://github.com/wildsmileofbutterfly/cryptocompy/archive/0.1.1.dev2.tar.gz',
       license='MIT',
       python_requires='>=3',
       install_requires=['requests'],)
